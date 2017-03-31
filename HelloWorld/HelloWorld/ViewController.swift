b1397fec87154e70ff962b41516e776ef66199e3
pattyzamsal <pazs1991@gmail.com>
2017-03-31 13:14:28 -0400

    Add documentation to explain the code

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -20,9 +20,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // Method that is called by the Interface Builder to show the alert
     @IBAction func showMessage() {
+        // create an object of UIAlertController type
         let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertControllerStyle.alert)
+        // Add action to the alert
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        // show the alert
         present(alertController, animated: true, completion: nil)
     }
 }

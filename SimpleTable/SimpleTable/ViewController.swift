5c1d036e771f05d22261fbb127ed0a8b9cad1a23
pattyzamsal <pazs1991@gmail.com>
2017-04-04 17:00:12 -0400

    Show rows in the table view with the name of the restaurants

--- SimpleTable/SimpleTable/ViewController.swift
+++ SimpleTable/SimpleTable/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    /*
+     *  UITableViewDataSource
+     */
+    
+    // this method inform the table view the total number of rows in a section
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows in the section. 
+        return restaurantNames.count
+    }
+    
+    // will be called every time a table row is displayed
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        // Configure the cell... 
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        return cell
+    }
 }
 

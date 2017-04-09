d9dc8dfb10fa0d48140f383c65b177323424a608
pattyzamsal <pazs1991@gmail.com>
2017-04-08 22:02:05 -0400

    Add two actions to the sheet alert

--- FoodPin/FoodPin/RestaurantTableTableViewController.swift
+++ FoodPin/FoodPin/RestaurantTableTableViewController.swift
@@ -71,6 +71,27 @@ class RestaurantTableTableViewController: UITableViewController {
         // Add actions to the menu 
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         optionMenu.addAction(cancelAction)
+        
+        // Add Call action
+        let callActionHandler = { (action:UIAlertAction!) -> Void in
+            let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry, the call feature is not available yet. Please retry later.", preferredStyle: .alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alertMessage, animated: true, completion: nil)
+        }
+        let callAction = UIAlertAction(title: "Call " + "123-000-\(indexPath.row)", style: .default, handler: callActionHandler)
+        optionMenu.addAction(callAction)
+        
+        // Check-in actionlet
+        let checkInAction = UIAlertAction(title: "Check in", style: .default, handler:
+            {
+                (action:UIAlertAction!) -> Void in
+                
+                let cell = tableView.cellForRow(at: indexPath)
+                cell?.accessoryType = .checkmark
+            })
+        
+        optionMenu.addAction(checkInAction)
+        
         // Display the menu 
         present(optionMenu, animated: true, completion: nil)
     }

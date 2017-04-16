937f7ccacca784ef399c8e4f3c1bb6bb6d556570
pattyzamsal <pazs1991@gmail.com>
2017-04-16 11:33:29 -0400

    Create segue to pass the image to the detail view

--- FoodPin/FoodPin/RestaurantTableTableViewController.swift
+++ FoodPin/FoodPin/RestaurantTableTableViewController.swift
@@ -202,14 +202,20 @@ class RestaurantTableTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "showRestaurantDetail" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let destinationController = segue.destination as! RestaurantDetailViewController
+                destinationController.restaurantImage = restaurantImages[indexPath.row]
+            }
+        }
     }
-    */
+    
 
 }

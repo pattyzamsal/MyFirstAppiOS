4ad69c0815217abc14662ddc2c5ad815a58923a4
pattyzamsal <pazs1991@gmail.com>
2017-04-16 11:28:58 -0400

    Create controller to the detail view

---
+++ FoodPin/FoodPin/RestaurantDetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  RestaurantDetailViewController.swift
+//  FoodPin
+//
+//  Created by Devstn1 on 2017-04-16.
+//  Copyright © 2017 Solsteace. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailViewController: UIViewController {
+
+    @IBOutlet weak var RestaurantImageView: UIImageView!
+    var restaurantImage = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        restaurantImageView.image = UIImage(named: restaurantImage)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

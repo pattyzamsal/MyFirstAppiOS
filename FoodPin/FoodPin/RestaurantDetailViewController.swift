d3338bb623ca1af2c30ad01f5d43cc75fd727e1d
pattyzamsal <pazs1991@gmail.com>
2017-04-16 17:26:25 -0400

    Enable self sizing cells

--- FoodPin/FoodPin/RestaurantDetailViewController.swift
+++ FoodPin/FoodPin/RestaurantDetailViewController.swift
@@ -32,6 +32,10 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
         tableView.separatorColor = UIColor(red: 240.0/255.0, green: 240.0/255.0, blue: 240.0/255.0, alpha: 0.8)
         // Assign title to the navigation bar
         title = restaurant.name
+        
+        // enable self sizing cells
+        tableView.estimatedRowHeight = 36.0
+        tableView.rowHeight = UITableViewAutomaticDimension
     }
 
     override func didReceiveMemoryWarning() {

8d92dcec6c97c70721146e395c72bfe04a3683df
pattyzamsal <pazs1991@gmail.com>
2017-04-16 15:46:12 -0400

    Add title of the detail view

--- FoodPin/FoodPin/RestaurantDetailViewController.swift
+++ FoodPin/FoodPin/RestaurantDetailViewController.swift
@@ -23,6 +23,8 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
         tableView.backgroundColor = UIColor(red: 240.0/255.0, green: 240.0/255.0, blue: 240.0/255.0, alpha: 0.2)
         tableView.tableFooterView = UIView(frame: CGRect.zero)
         tableView.separatorColor = UIColor(red: 240.0/255.0, green: 240.0/255.0, blue: 240.0/255.0, alpha: 0.8)
+        // Assign title to the navigation bar
+        title = restaurant.name
     }
 
     override func didReceiveMemoryWarning() {

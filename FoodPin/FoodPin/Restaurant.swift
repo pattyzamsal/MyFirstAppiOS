b94104ee1f695d3004062fff6cf8b7369beaf3b3
pattyzamsal <pazs1991@gmail.com>
2017-04-16 12:07:15 -0400

    Create class Restaurant

---
+++ FoodPin/FoodPin/Restaurant.swift
@@ -0,0 +1,26 @@
+//
+//  Restaurant.swift
+//  FoodPin
+//
+//  Created by Devstn1 on 2017-04-16.
+//  Copyright © 2017 Solsteace. All rights reserved.
+//
+
+import Foundation
+
+class Restaurant {
+    var name = ""
+    var type = ""
+    var location = ""
+    var image = ""
+    var isVisited = false
+    
+    // Constructor
+    init(name: String, type: String, location: String, image: String, isVisited: Bool) {
+        self.name = name
+        self.type = type
+        self.location = location
+        self.image = image
+        self.isVisited = isVisited
+    }
+}

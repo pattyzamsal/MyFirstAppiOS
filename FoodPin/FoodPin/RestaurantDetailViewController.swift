f462280dfbd2d0ecd855badac3b6d0f2f19f915b
pattyzamsal <pazs1991@gmail.com>
2017-04-16 21:03:30 -0400

    Show rating at the detail view

--- FoodPin/FoodPin/RestaurantDetailViewController.swift
+++ FoodPin/FoodPin/RestaurantDetailViewController.swift
@@ -66,7 +66,9 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
             cell.valueLabel.text = restaurant.phone
         case 4:
             cell.fieldLabel.text = "Been here"
-            cell.valueLabel.text = (restaurant.isVisited) ? "Yes, I've been here before" : "No" default: cell.fieldLabel.text = ""
+            cell.valueLabel.text = (restaurant.isVisited) ? "Yes, I've been here before. \(restaurant.rating)" : "No"
+        default:
+            cell.fieldLabel.text = ""
             cell.valueLabel.text = ""
         }
         cell.backgroundColor = UIColor.clear
@@ -88,5 +90,19 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
     }
     // unwind segue to rating the restaurant
     @IBAction func ratingButtonTapped(segue: UIStoryboardSegue) {
+        if let rating = segue.identifier {
+            restaurant.isVisited = true
+            switch rating {
+            case "great":
+                restaurant.rating = "Absolutely love it! Must try."
+            case "good":
+                restaurant.rating = "Pretty good."
+            case "dislike":
+                restaurant.rating = "I don't like it."
+            default:
+                break
+            }
+        }
+        tableView.reloadData()
     }
 }
